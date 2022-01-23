a2c4e7c50181ff222f83244ffeefad571eff9f8e
Ewout van Driel <ewout2000@gmail.com>
2022-01-23 14:52:35 +0100

    aanpassen, beoordelen en overzicht toegevoegd

---
+++ src/components/overzicht/vak-overzicht.js
@@ -0,0 +1,106 @@
+import {LitElement, html, css} from 'lit';
+
+class vakOverzicht extends LitElement {
+    static get styles() {
+        return css`
+            
+        `;
+    }
+
+    render() {
+        return html`
+            <link href="styles.css" rel="stylesheet">
+            <body class="d-flex flex-column">
+<main class="flex-shrink-0">
+    <!-- Navigatie-->
+    <div id="nav-placeholder"></div>
+    <!-- Header-->
+    <header class="bg-dark py-5">
+        <div class="container px-5">
+            <div class="row gx-5 align-items-center justify-content-center">
+                <div class="col-lg-8 col-xl-7 col-xxl-6">
+                    <div class="my-5 text-center text-xl-start">
+                        <h1 class="display-5 fw-bolder text-white mb-2">Vakken overzicht</h1>
+                    </div>
+                </div>
+                <div class="col-xl-5 col-xxl-6 d-none d-xl-block text-center"></div>
+            </div>
+        </div>
+    </header>
+    <!-- Tabel of dergelijke-->
+
+    <div class="col-xs-12" style="height:50px;"></div>
+
+    <div class="container">
+        <table class="table table-bordered table-striped">
+            <thead>
+            <tr>
+                <th>Naam vak</th>
+                <th>Aanpassen</th>
+            </tr>
+            </thead>
+            <tbody id="myTable">
+            <tr>
+                <td>Businessplan Principles 1</td>
+                <td>
+                    <div class="button-box col-lg-12">
+                        <a href="vakaanpassen.html">
+                            <button class="btn btn-primary btn-lg" id="aanpasButton" type="submit">Aanpassen</button>
+                        </a>
+                    </div>
+                </td>
+            </tr>
+            <tr>
+                <td>Persoonlijke professionalisering</td>
+                <td>
+                    <div class="button-box col-lg-12">
+                        <a href="vakaanpassen.html">
+                            <button class="btn btn-primary btn-lg" id="aanpasButton" type="submit">Aanpassen</button>
+                        </a>
+                    </div>
+                </td>
+            </tr>
+            <tr>
+                <td>Businessplan Principles 2</td>
+                <td>
+                    <div class="button-box col-lg-12">
+                        <a href="vakaanpassen.html">
+                            <button class="btn btn-primary btn-lg" id="aanpasButton" type="submit">Aanpassen</button>
+                        </a>
+                    </div>
+                </td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
+    <br><br>
+</main>
+<!-- Footer-->
+<div id="footer-placeholder"></div>
+
+<script src="//code.jquery.com/jquery.min.js"></script>
+
+<script>
+    // navbar inladen
+    $.get("./holders/navbar.html", function(data){
+        $("#nav-placeholder").replaceWith(data);
+    });
+
+    // tabel inhoud inladen
+    $.get("./holders/footer.html", function(data){
+        $("#footer-placeholder").replaceWith(data);
+    });
+</script>
+<!-- Bootstrap JS-->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+<!--JS-->
+<script src="js/scripts.js"></script>
+</body>
+        `
+    }
+
+    _newEvent(event) {
+        this.dispatchEvent(new CustomEvent(event));
+    }
+}
+window.customElements.define('vak-overzicht', vakOverzicht);
--- src/misc/routes.js
+++ src/misc/routes.js
@@ -1,7 +1,11 @@
 import { Router } from '@vaadin/router';
 import '../components/cursus/cursus-div.js';
 import '../components/layout/test-comp.js';
-import '../components/searchfilter/home-table.js'
+import '../components/searchfilter/home-table.js';
+import '../components/overzicht/vak-overzicht';
+import '../components/overzicht/voorstel-overzicht';
+import '../components/aanpassen/vak-aanpassen';
+import '../components/beoordelen/voorstel-beoordelen';
 
 const outlet = document.querySelector('#main');
 const router = new Router(outlet);
@@ -10,6 +14,10 @@ const router = new Router(outlet);
 router.setRoutes([
     {path: '/', component: 'home-table'},
     {path: '/cursus/:code', component: 'cursus-div'},
+    {path: '/vakAanpassen', component: 'vak-aanpassen'},
+    {path: '/voorstelBeoordelen', component: 'voorstel-beoordelen'},
+    {path: '/voorstelOverzicht', component: 'voorstel-overzicht'},
+    {path: '/vakOverzicht', component: 'vak-overzicht'},
     {path: '(.*)', component: ''}
 ]);
 
