73961131bb37690912d37c8b980c6e8144346020
Ewout2000-Ghub <ewout2000@gmail.com>
2022-01-25 00:25:39 +0100

    commit

---
+++ src/components/aanmaken/nieuwvak.js
@@ -0,0 +1,28 @@
+const nieuwvak = [
+    {
+    "code": "MBBM-P-DIP4-20",
+    "name": "pogramming",
+    "ec_course": 5,
+    "tests":[
+        {
+            "name": "Toets",
+            "weight": 100,
+            "ec_test":5,
+        }
+        ]
+    },
+    {
+        "code": "hshsp-fjh-364",
+        "name": "jan",
+        "ec_course": 5,
+        "tests":[
+            {
+                "name": "Toets",
+                "weight": 100,
+                "ec_test":5,
+            }
+            ]
+        },
+];
+
+export { nieuwvak };
---
+++ src/components/keuren/button-event.js
@@ -0,0 +1,11 @@
+function goedkeuren(){
+    document.getElementById("code").style.background="green";
+}
+
+function afkeuren(){
+    document.getElementById("code").style.background="red";
+}
+
+function aanpassen(){
+    window.location.href = "http://localhost:8080/vakaanpassen";
+}
